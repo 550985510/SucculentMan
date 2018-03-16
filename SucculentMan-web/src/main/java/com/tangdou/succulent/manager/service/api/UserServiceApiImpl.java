aebf181089554a02bc2dceed0ead440d17b900c3
木叶丸 <cui.hang@zhongfl.com>
2018-03-16 11:41:31 +0800

    目录结构

---
+++ SucculentMan-web/src/main/java/com/tangdou/succulent/manager/service/api/UserServiceApiImpl.java
@@ -0,0 +1,12 @@
+package com.tangdou.succulent.manager.service.api;
+
+import com.tangdou.succulent.manager.api.user.UserServiceApi;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 木叶丸
+ * @date 2018/3/16
+ */
+@Service
+public class UserServiceApiImpl implements UserServiceApi {
+}
